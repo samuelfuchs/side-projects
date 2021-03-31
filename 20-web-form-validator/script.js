5842828c1af43fa8df440d37d2aa7724417a5edb
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-31 03:59:55 -0300

    Check Length, Email & Password Match part 1

--- 20-web-form-validator/script.js
+++ 20-web-form-validator/script.js
@@ -19,9 +19,13 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
 	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-	return re.test(String(email).toLowerCase())
+	if (re.test(input.value.trim())) {
+		showSuccess(input)
+	} else {
+		showError(input, 'Email is not valid')
+	}
 }
 
 // Check required fields
@@ -35,6 +39,23 @@ function checkRequired(inputArr) {
 	})
 }
 
+// Check input length
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(
+			input,
+			`${getFieldname(input)} must be at lease ${min} characters`
+		)
+	} else if (input.value.length > max) {
+		showError(
+			input,
+			`${getFieldname(input)} must be less than ${max} characters`
+		)
+	} else {
+		showSuccess(input)
+	}
+}
+
 // Get Fieldname
 function getFieldname(input) {
 	return input.id.charAt(0).toUpperCase() + input.id.slice(1)
@@ -45,4 +66,7 @@ form.addEventListener('submit', function (e) {
 	e.preventDefault()
 
 	checkRequired([username, email, password, password2])
+	checkLength(username, 3, 15)
+	checkLength(password, 6, 25)
+	checkEmail(email)
 })
