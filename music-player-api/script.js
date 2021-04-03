86e1f858b06ea134bcb71e1960e4e5f3a9b0a316
Samuel Fuchs <fuchsgerson@gmail.com>
2021-04-03 06:47:57 -0300

    script update
    Brought in all the elements

---
+++ music-player-api/script.js
@@ -0,0 +1,11 @@
+const musicContainer = document.getElementById('music_container')
+
+const playBtn = document.getElementById('play')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+
+const audio = document.getElementById('audio')
+const progress = document.getElementById('progress')
+const progressContainer = document.getElementById('progress_container')
+const title = document.getElementById('title')
+const cover = document.getElementById('cover')
