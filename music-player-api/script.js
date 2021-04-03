5171db3ec6ea8b500011fa65418605af0e3ebf9b
Samuel Fuchs <fuchsgerson@gmail.com>
2021-04-03 07:00:25 -0300

    Load, Play & Pause Song
    Play and Pause buttons function

--- music-player-api/script.js
+++ music-player-api/script.js
@@ -1,11 +1,55 @@
-const musicContainer = document.getElementById('music_container')
-
+const musicContainer = document.getElementById('music-container')
 const playBtn = document.getElementById('play')
 const prevBtn = document.getElementById('prev')
 const nextBtn = document.getElementById('next')
 
 const audio = document.getElementById('audio')
 const progress = document.getElementById('progress')
-const progressContainer = document.getElementById('progress_container')
+const progressContainer = document.getElementById('progress-container')
 const title = document.getElementById('title')
 const cover = document.getElementById('cover')
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele']
+
+// Keep track of song
+let songIndex = 2
+
+// Initially load song details into DOM
+loadSong(songs[songIndex])
+
+// Update song details
+function loadSong(song) {
+	title.innerText = song
+	audio.src = `music/${song}.mp3`
+	cover.src = `images/${song}.jpg`
+}
+
+//Play Song
+function playSong() {
+	musicContainer.classList.add('play')
+	playBtn.querySelector('i.fas').classList.remove('fa-play')
+	playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+	audio.play()
+}
+
+// Pause Song
+function pauseSong() {
+	musicContainer.classList.remove('play')
+	playBtn.querySelector('i.fas').classList.add('fa-play')
+	playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+	audio.pause()
+}
+
+// Event Listeners
+playBtn.addEventListener('click', () => {
+	const isPlaying = musicContainer.classList.contains('play')
+
+	if (isPlaying) {
+		pauseSong()
+	} else {
+		playSong()
+	}
+})
