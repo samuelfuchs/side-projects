8a8f26c482bf8a5b95e7a039e29174d91f0f81b3
Samuel Fuchs <fuchsgerson@gmail.com>
2021-04-28 00:36:08 -0300

    Data Types

---
+++ 01-Fundamentals-Part-1-starter/assignments.js
@@ -0,0 +1,10 @@
+// Values and Variables
+let country = 'Brazil'
+let continent = 'South America'
+let population = '211'
+
+// Data Types
+let isIsland = false
+let language
+
+console.log(isIsland, population, country, language, continent)
--- 01-Fundamentals-Part-1-starter/script.js
+++ 01-Fundamentals-Part-1-starter/script.js
@@ -1,3 +1,4 @@
+/*
 // JavaScript Fundamentals - Part 1
 
 let js = 'amazing'
@@ -13,5 +14,24 @@ let continent = 'South America'
 let population = '211'
 
 console.log(country, continent, population)
-
+*/
 // Data Types
+let javascriptIsFun = true
+console.log(javascriptIsFun)
+
+// console.log(typeof true)
+console.log(typeof javascriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof '23')
+
+javascriptIsFun = 'YES!'
+console.log(javascriptIsFun)
+
+let year
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+console.log(typeof year)
+
+console.log(typeof null)
