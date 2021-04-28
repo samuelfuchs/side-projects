bab598c7dd18d4889a864fd3ae90e1fb508aa7b0
Samuel Fuchs <fuchsgerson@gmail.com>
2021-04-28 00:23:17 -0300

    Values and Variables

--- 01-Fundamentals-Part-1-starter/script.js
+++ 01-Fundamentals-Part-1-starter/script.js
@@ -1,4 +1,17 @@
+// JavaScript Fundamentals - Part 1
+
 let js = 'amazing'
-if (js === 'amazing') alert('JavaScript is FuN!')
 
 console.log(40 + 8 + 23 - 10)
+
+let firstName = 'Samuel'
+console.log(firstName)
+
+// Values and Variables
+let country = 'Brazil'
+let continent = 'South America'
+let population = '211'
+
+console.log(country, continent, population)
+
+// Data Types
